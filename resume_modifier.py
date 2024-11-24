000ac1d0b3c6aae42623be2d27b0cb09a1e820f2
sagar <sagar@securly.com>
2024-11-24 02:51:39 -0600

    Fix issue of first page not showing up

--- resume_modifier.py
+++ resume_modifier.py
@@ -6,7 +6,6 @@ import pdfkit
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from groq import Groq
-#from weasyprint import HTML
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
@@ -18,16 +17,13 @@ def convert_pdf_to_markdown(resume_pdf):
     with open(resume_pdf, "rb") as pdf_file:
         # Create a PDF reader object
         pdf_reader = PyPDF2.PdfReader(pdf_file)
-
         # Get the number of pages
         num_pages = len(pdf_reader.pages)
-
         # Extract text from each page
+        text = ""
         for page_num in range(num_pages):
             page = pdf_reader.pages[page_num]
-            text = page.extract_text()
-            #print(text)
-
+            text = text + page.extract_text()
     # Convert PDF to Markdown
     chat_completion = client.chat.completions.create(
         messages=[
@@ -36,9 +32,9 @@ def convert_pdf_to_markdown(resume_pdf):
                 "content": "Can you convert my resume to markdown? " + text + ".",
             }
         ],
-        #model="llama3-8b-8192",
+        model="llama3-8b-8192",
         #model="llama-3.2-3b-preview",
-        model="llama-3.1-70b-versatile",
+        #model="llama-3.1-70b-versatile",
     )
     return chat_completion.choices[0].message.content
 
@@ -50,9 +46,9 @@ def adapt_markdown(markdown, jd):
                 "content": "I have a resume in Markdown format and a job description. Please adapt my resume to better align with the job description. Ensure the language reflects the key skills and responsibilities from the job posting while retaining the structure of my resume. Tailor bullet points to demonstrate direct relevance to the job requirements. Highlight transferable skills and experience that make me an excellent candidate. And do not add your note in the output. \n Here is my resume:\n " + markdown + "\n\n Here is the job description:\n" + jd + ".",
             }
         ],
-        #model="llama3-8b-8192",
+        model="llama3-8b-8192",
         #model="llama-3.2-3b-preview",
-        model="llama-3.1-70b-versatile",
+        #model="llama-3.1-70b-versatile",
         #temperature=1,
     )
     return chat_completion.choices[0].message.content
@@ -82,16 +78,15 @@ def convert_markdown_to_html(adapted_markdown):
                 "content": "Can you convert resume (markdown) to html? \n" + adapted_markdown + ".",
             }
         ],
-        #model="llama3-8b-8192",
+        model="llama3-8b-8192",
         #model="llama-3.2-3b-preview",
-        model="llama-3.1-70b-versatile",
+        #model="llama-3.1-70b-versatile",
     )
     return chat_completion.choices[0].message.content
 
 def convert_html_to_pdf(html):
     # Convert Markdown to html
     pdfkit.from_string(html, 'output_resume.pdf')
-    #HTML(string=html).write_pdf('output.pdf')
 
 markdown = convert_pdf_to_markdown(resume_pdf)
 jd = get_jd(jd_link)
